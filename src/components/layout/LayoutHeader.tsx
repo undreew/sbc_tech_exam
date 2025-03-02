4dd51e5b8c4c84b30e17496dc12d2ac7d35e1500
james <jamesdejesus74@gmail.com>
2025-03-02 22:56:17 +0800

    reverted layout header

--- src/components/layout/LayoutHeader.tsx
+++ src/components/layout/LayoutHeader.tsx
@@ -1,37 +1,27 @@
 import React from 'react';
-import {useRouter} from 'next/router';
+import Link from 'next/link';
 
 import {Home} from '@mui/icons-material';
+import {Toolbar, Typography} from '@mui/material';
 import {AppBar, Box, Container} from '@mui/material';
-import {IconButton, Toolbar, Typography} from '@mui/material';
 
 import {FilterSearch} from '../filters';
-import {ROUTES} from '@/constants/routes';
 
 function LayoutHeader() {
-	const router = useRouter();
-
-	function handleRedirect() {
-		router.push({
-			pathname: ROUTES.LANDING.INDEX,
-			query: router.query,
-		});
-	}
-
 	return (
 		<AppBar position='fixed' elevation={0}>
 			<Container maxWidth='lg'>
 				<Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
 					<Typography
+						href='/'
 						variant='h6'
+						component={Link}
 						sx={{
 							color: 'white',
 							textDecoration: 'none',
 						}}
 					>
-						<IconButton onClick={handleRedirect}>
-							<Home />
-						</IconButton>
+						<Home />
 					</Typography>
 					<Box sx={{display: 'flex', gap: 1}}>
 						<FilterSearch />
